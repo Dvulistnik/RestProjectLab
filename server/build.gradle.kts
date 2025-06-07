1831d88ae3bb04676148e8e7e581e940e2e0c381
dvulist <dvulistnik@gmail.com>
2025-06-07 15:19:27 +0300

    Файлы запуска

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -12,6 +12,18 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
 }
 
+tasks {
+    shadowJar {
+        archiveFileName.set("rest-server.jar")
+
+        manifest {
+            attributes(mapOf("Main-Class" to application.mainClass.get()))
+        }
+
+        mergeServiceFiles()
+    }
+}
+
 dependencies {
     implementation(projects.shared)
     implementation(libs.logback)
@@ -21,7 +33,7 @@ dependencies {
     testImplementation(libs.kotlin.test.junit)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
-    implementation("org.jetbrains.exposed:exposed-core:0.48.0") // Используйте последнюю версию
+    implementation("org.jetbrains.exposed:exposed-core:0.48.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.48.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.48.0")
     implementation("org.jetbrains.exposed:exposed-java-time:0.48.0")
